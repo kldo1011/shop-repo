c0dc77ec6f2c8bbcbe05dffed28e1d21913f3a2e
Christian Kalmbacher <kach1021@hs-karlsruhe.de>
2013-10-30 16:22:13 +0100

    Artikel verändert

---
+++ shop/src/main/java/Artikelverwaltung/Artikel.java
@@ -0,0 +1,217 @@
+package Artikelverwaltung;
+
+import static javax.persistence.TemporalType.TIMESTAMP;
+
+import java.io.Serializable;
+import java.lang.invoke.MethodHandles;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.PostPersist;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+import org.jboss.logging.Logger;
+
+
+/**
+ * @author <a href="mailto:Juergen.Zimmermann@HS-Karlsruhe.de">J&uuml;rgen Zimmermann</a>
+ */
+@Entity
+@Table(indexes = @Index(columnList = "bezeichnung"))
+@NamedQueries({
+	@NamedQuery(name  = Artikel.FIND_VERFUEGBARE_ARTIKEL,
+            	query = "SELECT      a"
+            	        + " FROM     Artikel a"
+						+ " WHERE    a.ausgesondert = FALSE"
+                        + " ORDER BY a.id ASC"),
+	@NamedQuery(name  = Artikel.FIND_ARTIKEL_BY_BEZ,
+            	query = "SELECT      a"
+                        + " FROM     Artikel a"
+						+ " WHERE    a.bezeichnung LIKE :" + Artikel.PARAM_BEZEICHNUNG
+						+ "          AND a.ausgesondert = FALSE"
+			 	        + " ORDER BY a.id ASC"),
+   	@NamedQuery(name  = Artikel.FIND_ARTIKEL_MAX_PREIS,
+            	query = "SELECT      a"
+                        + " FROM     Artikel a"
+						+ " WHERE    a.preis < :" + Artikel.PARAM_PREIS
+			 	        + " ORDER BY a.id ASC")
+})
+@XmlRootElement
+public class Artikel implements Serializable {
+	private static final long serialVersionUID = -3700579190995722151L;
+	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass());
+	
+	private static final int BEZEICHNUNG_LENGTH_MAX = 32;
+	
+	private static final String PREFIX = "Artikel.";
+	public static final String FIND_VERFUEGBARE_ARTIKEL = PREFIX + "findVerfuegbareArtikel";
+	public static final String FIND_ARTIKEL_BY_BEZ = PREFIX + "findArtikelByBez";
+	public static final String FIND_ARTIKEL_MAX_PREIS = PREFIX + "findArtikelByMaxPreis";
+
+	public static final String PARAM_BEZEICHNUNG = "bezeichnung";
+	public static final String PARAM_PREIS = "preis";
+
+	@Id
+	@GeneratedValue
+	@Column(nullable = false, updatable = false)
+	private Long id = KEINE_ID;
+	
+	@Column(length = BEZEICHNUNG_LENGTH_MAX, nullable = false)
+	@NotNull(message = "{artikel.bezeichnung.notNull}")
+	@Size(max = BEZEICHNUNG_LENGTH_MAX, message = "{artikel.bezeichnung.length}")
+	private String bezeichnung = "";
+	
+	@Column(precision = 8, scale = 2)
+	private BigDecimal preis;
+	
+	private boolean ausgesondert;
+	
+	@Basic(optional = false)
+	@Temporal(TIMESTAMP)
+	@XmlTransient
+	private Date erzeugt;
+
+	@Basic(optional = false)
+	@Temporal(TIMESTAMP)
+	@XmlTransient
+	private Date aktualisiert;
+	
+	public Artikel() {
+		super();
+	}
+	
+	public Artikel(String bezeichnung, BigDecimal preis) {
+		super();
+		this.bezeichnung = bezeichnung;
+		this.preis = preis;
+	}
+
+	@PrePersist
+	private void prePersist() {
+		erzeugt = new Date();
+		aktualisiert = new Date();
+	}
+	
+	@PostPersist
+	private void postPersist() {
+		LOGGER.debugf("Neuer Artikel mit ID=%d", id);
+	}
+	
+	@PreUpdate
+	private void preUpdate() {
+		aktualisiert = new Date();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getBezeichnung() {
+		return bezeichnung;
+	}
+
+	public void setBezeichnung(String bezeichnung) {
+		this.bezeichnung = bezeichnung;
+	}
+
+	public void setPreis(BigDecimal preis) {
+		this.preis = preis;
+	}
+
+	public BigDecimal getPreis() {
+		return preis;
+	}
+
+	public boolean isAusgesondert() {
+		return ausgesondert;
+	}
+
+	public void setAusgesondert(boolean ausgesondert) {
+		this.ausgesondert = ausgesondert;
+	}
+
+	public Date getErzeugt() {
+		return erzeugt == null ? null : (Date) erzeugt.clone();
+	}
+
+	public void setErzeugt(Date erzeugt) {
+		this.erzeugt = erzeugt == null ? null : (Date) erzeugt.clone();
+	}
+
+	public Date getAktualisiert() {
+		return aktualisiert == null ? null : (Date) aktualisiert.clone();
+	}
+
+	public void setAktualisiert(Date aktualisiert) {
+		this.aktualisiert = aktualisiert == null ? null : (Date) aktualisiert.clone();
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (ausgesondert ? 1231 : 1237);
+		result = prime * result
+				+ ((bezeichnung == null) ? 0 : bezeichnung.hashCode());
+		result = prime * result + ((preis == null) ? 0 : preis.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Artikel other = (Artikel) obj;
+		if (ausgesondert != other.ausgesondert)
+			return false;
+		if (bezeichnung == null) {
+			if (other.bezeichnung != null) {
+				return false;
+			}
+		}
+		else if (!bezeichnung.equals(other.bezeichnung)) {
+			return false;
+		}
+		if (preis == null) {
+			if (other.preis != null) {
+				return false;
+			}
+		}
+		else if (!preis.equals(other.preis)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Artikel [id=" + id + ", bezeichnung=" + bezeichnung
+		       + ", preis=" + preis + ", ausgesondert=" + ausgesondert
+		       + ", erzeugt=" + erzeugt
+			   + ", aktualisiert=" + aktualisiert + "]";
+	}
+}
+
