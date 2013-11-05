4dc12dfb304d58db7083ce11e252afb4531f25bd
Sebastian Bönig <sboenig(at)web.de>
2013-11-05 15:55:33 +0100

    Artikel-Update(Lieferant)

--- shop/src/main/java/Artikelverwaltung/domain/Artikel.java
+++ shop/src/main/java/Artikelverwaltung/domain/Artikel.java
@@ -1,16 +1,18 @@
 package Artikelverwaltung.domain;
 
 import java.net.URI;
+
 import javax.xml.bind.annotation.XmlRootElement;
 
+import Kundenverwaltung.domain.Lieferant;
+
 @XmlRootElement
-public class Artikel 
-{
+public class Artikel {
 	private URI artikelUri;
 	private String bezeichnung;
 	private Long preis;
 	private Long id;
-	
+	private Lieferant lieferant;
 
 	public URI getArtikelUri() {
 		return artikelUri;
@@ -35,20 +37,38 @@ public class Artikel
 	public Long getPreis() {
 		return preis;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
+	public Lieferant getLieferant() {
+		return lieferant;
+	}
+
+	public void setLieferant(Lieferant lieferant) {
+		this.lieferant = lieferant;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result
+				+ ((artikelUri == null) ? 0 : artikelUri.hashCode());
+		result = prime * result
+				+ ((bezeichnung == null) ? 0 : bezeichnung.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((lieferant == null) ? 0 : lieferant.hashCode());
+		result = prime * result + ((preis == null) ? 0 : preis.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -58,21 +78,33 @@ public class Artikel
 		if (getClass() != obj.getClass())
 			return false;
 		Artikel other = (Artikel) obj;
+		if (artikelUri == null) {
+			if (other.artikelUri != null)
+				return false;
+		} else if (!artikelUri.equals(other.artikelUri))
+			return false;
+		if (bezeichnung == null) {
+			if (other.bezeichnung != null)
+				return false;
+		} else if (!bezeichnung.equals(other.bezeichnung))
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
+		if (lieferant == null) {
+			if (other.lieferant != null)
+				return false;
+		} else if (!lieferant.equals(other.lieferant))
+			return false;
+		if (preis == null) {
+			if (other.preis != null)
+				return false;
+		} else if (!preis.equals(other.preis))
+			return false;
 		return true;
 	}
-	@Override
-	public String toString() {
-		return "Artikel [id=" + id + "]";
-	}
-	
-	
-	
-	
 	
 	
 }
