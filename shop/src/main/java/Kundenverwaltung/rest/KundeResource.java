ae3ca3725b8de4454e8d9e17e13c26056704ad36
Dominik <kldo1011@hs-karlsruhe.de>
2013-11-05 13:20:27 +0100

    kunderesource bestellungresource

--- shop/src/main/java/Kundenverwaltung/rest/KundeResource.java
+++ shop/src/main/java/Kundenverwaltung/rest/KundeResource.java
@@ -1,5 +1,58 @@
 package Kundenverwaltung.rest;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.APPLICATION_XML;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+import static javax.ws.rs.core.MediaType.TEXT_XML;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.Link;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import util.rest.UriHelper;
+
+@Path("/kunden")
+@Produces({ APPLICATION_JSON, APPLICATION_XML + ";qs=0.75", TEXT_XML + ";qs=0.5" })
+@Consumes
 public class KundeResource {
+	
+	public static final String KUNDEN_ID_PATH_PARAM="kundeID";
+	public static final String KUNDEN_NACHNAME_QUERY_PARAM="nachname";
+	@Context
+	private UriInfo uriInfo;
+	
+	@Inject
+	private BestellungResource bestellungResource;
+	
+	@Inject
+	private UriHelper uriHelper;
+	
+	@GET
+	@Produces({ TEXT_PLAIN, APPLICATION_JSON })
+	@Path("version")
+	public String getVersion() {
+		return "1.0";
+	
+	
 
 }
