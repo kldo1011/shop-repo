0bf71fb832796fab929434a92dd3552941640f1e
Sebastian Bönig <sboenig(at)web.de>
2013-11-05 15:30:55 +0100

    Lieferant

---
+++ shop/src/main/java/Kundenverwaltung/domain/Lieferant.java
@@ -0,0 +1,103 @@
+package Kundenverwaltung.domain;
+
+import java.util.List;
+
+import Artikelverwaltung.domain.Artikel;
+
+public class Lieferant {
+	private Long id;
+	private String name;
+	private String email;
+	private Adressen adresse;
+	private List<Artikel> artikel;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Adressen getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(Adressen adresse) {
+		this.adresse = adresse;
+	}
+
+	public List<Artikel> getArtikel() {
+		return artikel;
+	}
+
+	public void setArtikel(List<Artikel> artikel) {
+		this.artikel = artikel;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((adresse == null) ? 0 : adresse.hashCode());
+		result = prime * result + ((artikel == null) ? 0 : artikel.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lieferant other = (Lieferant) obj;
+		if (adresse == null) {
+			if (other.adresse != null)
+				return false;
+		} else if (!adresse.equals(other.adresse))
+			return false;
+		if (artikel == null) {
+			if (other.artikel != null)
+				return false;
+		} else if (!artikel.equals(other.artikel))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+}
