1bafe8e2128358c58a94fc9c7711cf40a9f7d5b6
Moritz Bückle <bumo1011@hs-karlsruhe.de>
2013-12-03 14:24:54 +0100

    NotFoundException und Mapper überarbeitet

--- shop/src/main/java/util/rest/NotFoundException.java
+++ shop/src/main/java/util/rest/NotFoundException.java
@@ -1,14 +1,19 @@
 package util.rest;
 
-public class NotFoundException extends RuntimeException {
 
-	private static final long serialVersionUID = -866705588853138386L;
 
-	public NotFoundException(String msg) {
-		super(msg);
+	public class NotFoundException extends RuntimeException {
+		private static final long serialVersionUID = -866705588853138386L;
+		
+		private final Object[] args;
+
+		public NotFoundException(String msg, Object... args) {
+			super(msg);
+			this.args = args;
+		}
+
+		public Object[] getArgs() {
+			return args;
+		}
 	}
 
-	public NotFoundException(String msg, Throwable t) {
-		super(msg, t);
-	}
-}
