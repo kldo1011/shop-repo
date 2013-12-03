cc6567d76560d4dc54131580e53b69bee8341c9b
Moritz Bückle <bumo1011@hs-karlsruhe.de>
2013-12-03 14:11:22 +0100

    AbstractShopException

---
+++ shop/src/main/java/util/AbstractShopException.java
@@ -0,0 +1,20 @@
+package util;
+
+public abstract class AbstractShopException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2704616205253670445L;
+	
+	public AbstractShopException(String msg) {
+		super(msg);
+	}
+
+	public AbstractShopException(String msg, Throwable t) {
+		super(msg, t);
+	}
+
+	public abstract String getMessageKey();
+
+}
